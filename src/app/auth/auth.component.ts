481af194e6856c6e2f64b387c540288abd779fb2
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2025-02-18 15:52:34 +0530

    first commit

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,23 @@
+import {Component, computed, inject, signal} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {
+  email = signal('');
+  password = signal('');
+  private  authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
+
+  onSubmit() {
+    this.authService.authenticate(this.email(), this.password());
+  }
+}
---
+++ src/app/learning-resources/learning-resources.component.ts
@@ -0,0 +1,11 @@
+import {Component, signal} from '@angular/core';
+
+@Component({
+  selector: 'app-learning-resources',
+  templateUrl: './learning-resources.component.html',
+  styleUrl: './learning-resources.component.css',
+  standalone: true,
+})
+export class LearningResourcesComponent  {
+
+}
